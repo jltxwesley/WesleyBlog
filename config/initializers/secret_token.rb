7fb6809f18b406ddc2f7fea1ab5b3a450ad6fa1f
Wesley Zhang <jltxwesley@gmail.com>
2013-05-26 23:12:24 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WesleyBlog::Application.config.secret_token = '31640913da3a37fc18fac1b3b1ff30146d1d391c43d968ad4445cd09698e4341d1e0ac6a93cf247682c1793f81b4167f1ccc30ea24fa60927c48bd32efccdaa3'
